ba09a802a6b4a40d30c4f005a33e74c57ab30c61
Gabriel Camurça <gabriel@brtech.dev>
2023-11-25 12:58:56 -0300

    Create home 1/2

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Gitfind</h1>
+    </header>
+  )
+};
+
+export { Header };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,33 @@
+import { Header } from "../../components/Header";
+import background from '../../assets/background.png';
+
+import './styles.css';
+
 function App() {
   return (
     <div className="App">
-      Olá mundo!
+      <Header />
+      <div className="conteudo">
+        <img src={background} alt="logo-github" className="background"/>
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username"/>
+            <button>Buscar</button>  
+          </div>
+          <div className="perfil">
+            <img className="profile_img" src="https://avatars.githubusercontent.com/u/122840209?v=4" alt="imgProfile"/>
+            <div>
+              <h3>Nome do usuário</h3>
+              <span>@gabrielcamurcab</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
+          <div className="div_repositorios">
+            <h4>Repositórios</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
